e2832ae9e116ac2f15771fbc905a945bc92ec084
MrMoss12-cmd <eliashigueraacosta1@outlook.com>
2025-01-01 00:02:50 -0500

    especificiacion de homeview terminada

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:8080',
+    methods: 'GET,POST',
+    credentials: true,
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`Aplication is running on: http://localhost:${port}/graphql`);
+  console.log(`Application is running on: http://localhost:${port}`);
+  
 }
 bootstrap();
